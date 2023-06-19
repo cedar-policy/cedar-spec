13da5873a652f724bbbdb678f5d540a695830a9a
Craig Disselkoen <cdiss@amazon.com>
2023-06-19 11:34:49 -0400

    cedar-drt: Avoid using private-ish APIs `add_ancestor()` and `set_attr()` (#48)
    * avoid one use of Entity::add_ancestor
    
    * avoid a use of entity.set_attr

--- cedar-drt/fuzz/src/schema.rs
+++ cedar-drt/fuzz/src/schema.rs
@@ -660,7 +660,7 @@ impl Schema {
 
     /// Get an arbitrary Hierarchy conforming to the schema.
     pub fn arbitrary_hierarchy(&self, u: &mut Unstructured<'_>) -> Result<super::Hierarchy> {
-        // For each entity type in the schema, generate one or more entities of that type
+        // For each entity type in the schema, generate one or more entity UIDs of that type
         let uids_by_type = self
             .schema
             .entity_types
@@ -682,12 +682,7 @@ impl Schema {
                     .map(|uid| (uid.clone(), ast::Entity::with_uid(uid.clone())))
             })
             .collect::<HashMap<ast::EntityUID, ast::Entity>>();
-        let uids = entities.iter().map(|(uid, _)| uid.clone()).collect();
-        let mut hierarchy = super::Hierarchy {
-            entities,
-            uids,
-            uids_by_type,
-        };
+        let uids: Vec<ast::EntityUID> = entities.iter().map(|(uid, _)| uid.clone()).collect();
         let entitytypes_by_type: HashMap<ast::Name, &EntityType> = self
             .schema
             .entity_types
@@ -699,18 +694,30 @@ impl Schema {
                 )
             })
             .collect();
-        // for each entity, optionally add hierarchy-membership edges to every
-        // entity that's eligible to be a parent of that type
-        for (uid, entity) in &mut hierarchy.entities {
-            match uid.entity_type() {
+        let hierarchy_no_attrs = super::Hierarchy {
+            uids: uids.clone(),
+            entities: uids
+                .iter()
+                .map(|uid| (uid.clone(), ast::Entity::with_uid(uid.clone())))
+                .collect(),
+            uids_by_type,
+        };
+        // create an entity hierarchy composed of those entity UIDs, with some
+        // hierarchy-membership edges possibly added in positions where the
+        // schema allows a parent of that type
+        let entities = uids
+            .iter()
+            .map(|uid| match uid.entity_type() {
                 // entity data is generated with `arbitrary_uid_with_type`, which can never
                 // produce an unspecified entity
                 ast::EntityType::Unspecified => {
                     panic!("should not be possible to generate an unspecified entity")
                 }
                 ast::EntityType::Concrete(name) => {
+                    // choose parents for this entity
+                    let mut parents = HashSet::new();
                     for allowed_parent_typename in &entitytypes_by_type
-                        .get(name)
+                        .get(&name)
                         .expect("typename should have an EntityType")
                         .member_of_types
                     {
@@ -718,29 +725,18 @@ impl Schema {
                             self.namespace.clone(),
                             allowed_parent_typename,
                         );
-                        for possible_parent_uid in hierarchy
+                        for possible_parent_uid in hierarchy_no_attrs
                             .uids_by_type
                             .get(&allowed_parent_typename)
                             .expect("all typenames should be in the map")
                         {
                             if u.ratio::<u8>(1, 2)? {
-                                entity.add_ancestor(possible_parent_uid.clone());
+                                parents.insert(possible_parent_uid.clone());
                             }
                         }
                     }
-                }
-            }
-        }
-        // for each entity, add appropriate attributes
-        let hierarchy_no_attrs = hierarchy.clone();
-        for (uid, entity) in &mut hierarchy.entities {
-            match uid.entity_type() {
-                // entity data is generated with `arbitrary_uid_with_type`, which can never
-                // produce an unspecified entity
-                ast::EntityType::Unspecified => {
-                    panic!("should not be possible to generate an unspecified entity")
-                }
-                ast::EntityType::Concrete(name) => {
+                    // generate appropriate attributes for this entity
+                    let mut attrs = HashMap::new();
                     let attr_or_context = unwrap_attrs_or_context(
                         &entitytypes_by_type
                             .get(name)
@@ -756,7 +752,7 @@ impl Schema {
                                 Type::arbitrary_nonextension(u)?
                             };
                             let attr_name: String = u.arbitrary()?;
-                            entity.set_attr(
+                            attrs.insert(
                                 attr_name.into(),
                                 self.arbitrary_attr_value_for_type(
                                     &attr_type,
@@ -783,7 +779,7 @@ impl Schema {
                                 self.settings.max_depth,
                                 u,
                             )?;
-                            entity.set_attr(
+                            attrs.insert(
                                 attr.parse().expect(
                                     "all attribute names in the schema should be valid identifiers",
                                 ),
@@ -791,11 +787,21 @@ impl Schema {
                             );
                         }
                     }
+                    // create the actual ast::Entity object
+                    let entity = ast::Entity::new(
+                        uid.clone(),
+                        attrs.into_iter().collect(),
+                        parents.into_iter().collect(),
+                    );
+                    Ok((uid.clone(), entity))
                 }
-            }
-        }
-
-        Ok(hierarchy)
+            })
+            .collect::<Result<_>>()?;
+        Ok(super::Hierarchy {
+            entities,
+            uids,
+            uids_by_type: hierarchy_no_attrs.uids_by_type,
+        })
     }
     pub fn arbitrary_hierarchy_size_hint(_depth: usize) -> (usize, Option<usize>) {
         (0, None)
