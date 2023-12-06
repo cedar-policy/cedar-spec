0c91f6a1909a753bac456cfd5ef50367bcc90038
Kesha Hietala <khieta@amazon.com>
2023-12-06 16:57:13 +0000

    minor cleanup

--- build.sh
+++ build.sh
@@ -4,27 +4,32 @@
 git submodule update --init
 
 # Set environment variables
-cd cedar-drt && source ./set_env_vars.sh
+cd cedar-drt
+source ./set_env_vars.sh
 cd ..
 
-# Build the Dafny formalization and extract Java code
-cd cedar-dafny && make compile-difftest
-cd ..
+# Build the Dafny formalization and extract to Java code
+# cd cedar-dafny
+# make compile-difftest
+# cd ..
 
 # Build the Dafny Java wrapper
-cd cedar-dafny-java-wrapper && ./gradlew build dumpClasspath
+cd cedar-dafny-java-wrapper
+./gradlew build dumpClasspath
 cd ..
 
 # Build the Lean formalization and extract to static C libraries
-cd cedar-lean && \
-lake exe cache get && \
+cd cedar-lean
 lake build Cedar:static DiffTest:static Std:static Mathlib:static Qq:static Aesop:static ProofWidgets:static
 cd ..
 
 # Build DRT
 cd cedar-drt
 cargo build
-cd fuzz && RUSTFLAGS="--cfg=fuzzing" cargo build
 
 # Run integration tests
 cargo test -- --nocapture
+
+# Build inner fuzz crate
+cd fuzz && RUSTFLAGS="--cfg=fuzzing" cargo build
+cargo test
