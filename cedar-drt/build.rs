bd5d3e0aa0dc8bd2b020c160b928860534da0aa2
Kesha Hietala <khieta@amazon.com>
2023-12-13 15:22:40 +0000

    fix build

--- cedar-drt/build.rs
+++ cedar-drt/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 fn main() {
     let lean_dir = env::var("LEAN_LIB_DIR").unwrap();
-    println!("cargo:rustc-link-search=native=../cedar-lean/build/lib");
+    println!("cargo:rustc-link-search=native=../cedar-lean/.lake/build/lib");
     println!("cargo:rustc-link-search=native={lean_dir}");
-    println!("cargo:rustc-link-search=native=../cedar-lean/lake-packages/std/build/lib");
+    println!("cargo:rustc-link-search=native=../cedar-lean/.lake/packages/std/.lake/build/lib");
 }
