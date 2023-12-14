998cefec0656c36b8a86d5b21968ba6b078a1215
Kesha Hietala <khieta@amazon.com>
2023-12-14 16:17:57 +0000

    add back LD_PRELOAD

--- cedar-drt/set_env_vars.sh
+++ cedar-drt/set_env_vars.sh
@@ -31,3 +31,4 @@ fi
 export LEAN_LIB_DIR=$(lean --print-libdir)
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH+$LD_LIBRARY_PATH:}$(lean --print-libdir)
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH+$DYLD_LIBRARY_PATH:}$(lean --print-libdir)
+export LD_PRELOAD=${LD_PRELOAD+$LD_PRELOAD:}$(lean --print-prefix)/lib/glibc/libm.so
