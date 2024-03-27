0fc0e9292f0d7b8efd004104c5c84147b6b19a6e
Kesha Hietala <khieta@amazon.com>
2024-03-27 17:16:41 +0000

    more updates
    Signed-off-by: Kesha Hietala <khieta@amazon.com>

--- cedar-drt/set_env_vars.sh
+++ cedar-drt/set_env_vars.sh
@@ -8,7 +8,7 @@ else
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH+$LD_LIBRARY_PATH:}$(lean --print-libdir)
     export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH+$DYLD_LIBRARY_PATH:}$(lean --print-libdir)
 
-    # if the version of GLIBC is too old (< 2.27), then use the verison of libm.so packaged with Lean
+    # if the version of GLIBC is too old (< 2.27), then use the version of libm.so packaged with Lean
     GLIBC_VERSION=`ldd --version | awk '/ldd/{print $NF}'`
     if awk "BEGIN {exit !($GLIBC_VERSION < 2.27)}"; then
         export LD_PRELOAD=${LD_PRELOAD+$LD_PRELOAD:}$(lean --print-prefix)/lib/glibc/libm.so
