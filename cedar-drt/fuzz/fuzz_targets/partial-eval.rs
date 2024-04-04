2d7d7148892127b02dcd4eb5f0c9062b441343a8
Aaron Eline <aeline+github@amazon.com>
2024-04-04 17:26:52 +0000

    Updating to use new PE api
    Co-authored-by: Kesha Hietala <kesha.hietala@gmail.com>
    Signed-off-by: Aaron Eline <aeline+github@amazon.com>

--- cedar-drt/fuzz/fuzz_targets/partial-eval.rs
+++ cedar-drt/fuzz/fuzz_targets/partial-eval.rs
@@ -15,13 +15,15 @@
  */
 
 #![no_main]
-use crate::authorizer::{Response, ResponseKind};
 use cedar_drt::*;
 use cedar_drt_inner::*;
 use cedar_policy_core::ast;
 use cedar_policy_core::ast::Policy;
+use cedar_policy_core::ast::PolicyID;
 use cedar_policy_core::ast::PolicySet;
-use cedar_policy_core::authorizer::Authorizer;
+use cedar_policy_core::ast::Value;
+use cedar_policy_core::authorizer::AuthorizationError;
+use cedar_policy_core::authorizer::{Authorizer, PartialResponse, Response};
 use cedar_policy_core::entities::Entities;
 use cedar_policy_generators::{
     abac::{ABACPolicy, ABACRequest},
@@ -34,7 +36,10 @@ use libfuzzer_sys::arbitrary::{self, Arbitrary, Unstructured};
 use log::debug;
 use serde::Serialize;
 use smol_str::SmolStr;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::convert::TryFrom;
+use std::sync::Arc;
 
 /// Input expected by this fuzz target:
 /// An ABAC hierarchy, policy, and 8 associated requests
@@ -123,6 +128,59 @@ fn responses_equiv(a1: Response, a2: Response) -> bool {
         && (a1.diagnostics.errors.is_empty() == a2.diagnostics.errors.is_empty())
 }
 
+/// Substitute all policies in the set according to the mapping
+fn substitute_set(p: &PolicySet, mapping: &HashMap<SmolStr, Value>) -> PolicySet {
+    let mut new_set = PolicySet::default();
+    for policy in p.policies() {
+        new_set
+            .add(substitute_policy(policy, mapping))
+            .expect("Failed to add substituted policy to set");
+    }
+
+    new_set
+}
+
+/// Substitute the condition clause of this policy according to the mapping
+fn substitute_policy(p: &Policy, mapping: &HashMap<SmolStr, Value>) -> Policy {
+    let condition = p.condition().substitute(mapping);
+    Policy::from_when_clause_annos(
+        p.effect(),
+        Arc::new(condition),
+        p.id().clone(),
+        p.annotations_arc().clone(),
+    )
+}
+
+fn get_id(e: &AuthorizationError) -> &PolicyID {
+    match e {
+        AuthorizationError::PolicyEvaluationError { id, .. } => id,
+    }
+}
+
+/// Check if a partial response is consistent with a concrete response
+fn partial_response_correctness(partial: &PartialResponse, concrete: &Response) {
+    let actually_errored: HashSet<_> = concrete.diagnostics.errors.iter().map(get_id).collect();
+    let partial_errored: HashSet<_> = partial.definitely_errored().collect();
+    // Ensure the errors in the concrete response are a superset of errors in the partial
+    // response
+    assert!(actually_errored.is_superset(&partial_errored));
+    let determining = &concrete.diagnostics.reason;
+    // Ensure that `may_be_determining` produces an over approximation of policies in the
+    // reasons set
+    let over_approx = partial
+        .may_be_determining()
+        .cloned()
+        .collect::<HashSet<_>>();
+    assert!(over_approx.is_superset(&determining));
+    // Ensure that `must_be_determining` produces an under approximation of policies in the
+    // reasons set
+    let under_approx = partial
+        .must_be_determining()
+        .cloned()
+        .collect::<HashSet<_>>();
+    assert!(determining.is_superset(&under_approx));
+}
+
 // The main fuzz target. This is for type-directed fuzzing of ABAC
 // hierarchy/policy/requests
 fuzz_target!(|input: FuzzTargetInput| {
@@ -146,32 +204,32 @@ fuzz_target!(|input: FuzzTargetInput| {
         let auth = Authorizer::new();
         let ans = auth.is_authorized_core(q.clone(), &policyset, &input.entities);
 
-        match ans {
-            ResponseKind::FullyEvaluated(ans) => {
-                // Concrete evaluation should also succeed w/out any substitutions
-                let concrete_res = auth.is_authorized(q, &policyset, &input.entities);
-                assert!(responses_equiv(ans, concrete_res));
-            }
-            ResponseKind::Partial(residual_set) => {
-                let mut subst_set = PolicySet::new();
-                for policy in residual_set.residuals.policies().map(|p: &Policy| {
-                    let subst = p.condition().substitute(&mapping).unwrap();
-                    Policy::from_when_clause(p.effect(), subst, p.id().clone())
-                }) {
-                    subst_set.add(policy).unwrap();
-                }
-                let final_res = auth.is_authorized(q.clone(), &subst_set, &input.entities);
+        if let Some(_decision) = ans.decision() {
+            // Concrete evaluation should also succeed w/out any substitutions
+            let concrete_res = auth.is_authorized(q, &policyset, &input.entities);
+            assert!(responses_equiv(ans.into(), concrete_res));
+        } else {
+            // If we are not able to reach a concrete decision:
+            // First we ensure that if we substitute the residuals and evaluate, we get a concrete
+            // answer consistent with the partial response
+            let concrete_response = ans
+                .reauthorize(&mapping, &auth, q.clone(), &input.entities)
+                .unwrap();
+            assert!(
+                concrete_response.decision().is_some(),
+                "Substituting all `unknown`s should give a decision"
+            );
+            let concrete: Response = concrete_response.into();
+            partial_response_correctness(&ans, &concrete);
 
-                let mut concrete_set = PolicySet::new();
-                for policy in policyset.policies().map(|p: &Policy| {
-                    let subst = p.condition().substitute(&mapping).unwrap();
-                    Policy::from_when_clause(p.effect(), subst, p.id().clone())
-                }) {
-                    concrete_set.add(policy).unwrap();
-                }
-                let concrete_res = auth.is_authorized(q, &concrete_set, &input.entities);
-                assert!(responses_equiv(concrete_res, final_res));
-            }
-        };
+            // Then we check if we substitute the original policies and evaluate, we get a concrete
+            // answer consistent with the partial answer
+            let substituted_start = substitute_set(&policyset, &mapping);
+            let final_answer = auth.is_authorized(q.clone(), &substituted_start, &input.entities);
+            partial_response_correctness(&ans, &final_answer);
+
+            // Then check that those two concrete responses are equivalent
+            assert!(responses_equiv(concrete, final_answer));
+        }
     }
 });
