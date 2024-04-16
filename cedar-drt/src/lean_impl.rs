ae6310e1d19396c366ee9e380638a3fa63533e20
Craig Disselkoen <cdiss@amazon.com>
2024-04-16 13:57:31 +0000

    update for cedar#760
    Signed-off-by: Craig Disselkoen <cdiss@amazon.com>

--- cedar-drt/src/lean_impl.rs
+++ cedar-drt/src/lean_impl.rs
@@ -28,6 +28,7 @@ use std::sync::Once;
 use crate::definitional_request_types::*;
 use cedar_policy_core::ast::{Expr, Value};
 pub use cedar_policy_core::*;
+use cedar_policy::frontend;
 use cedar_testing::cedar_test_impl::*;
 pub use lean_sys::init::lean_initialize;
 pub use lean_sys::lean_object;
@@ -181,7 +182,7 @@ impl LeanDefinitionalEngine {
                     })
                     .collect();
                 TestResult::Success(TestResponse {
-                    response: InterfaceResponse::new(decision, reason, errors),
+                    response: frontend::is_authorized::Response::new(decision, reason, errors),
                     timing_info: HashMap::from([("authorize".into(), Micros(auth_time))]),
                 })
             }
