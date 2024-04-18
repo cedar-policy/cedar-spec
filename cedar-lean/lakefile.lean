7fe718d911ff21e05e93a8609f057a4da3bf1aa7
Craig Disselkoen <cdiss@amazon.com>
2024-04-18 19:23:17 +0000

    lakefile changes from main
    Signed-off-by: Craig Disselkoen <cdiss@amazon.com>

--- cedar-lean/lakefile.lean
+++ cedar-lean/lakefile.lean
@@ -1,5 +1,5 @@
 /-
- Copyright 2022-2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Copyright Cedar Contributors
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ require std from git
 
 package Cedar
 
+@[default_target]
 lean_lib Cedar where
   defaultFacets := #[LeanLib.staticFacet]
 
