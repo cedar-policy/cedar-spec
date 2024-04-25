24e6d8d737d00e51592254d07eafb245d7e0ab58
Kesha Hietala <kesha.hietala@gmail.com>
2024-04-25 11:55:42 -0400

    add CI to autogenerate docs (#292)

--- cedar-lean/lakefile.lean
+++ cedar-lean/lakefile.lean
@@ -17,6 +17,9 @@
 import Lake
 open Lake DSL
 
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
 require std from git
   "https://github.com/leanprover/std4"@"main"
 
