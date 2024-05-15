2775168ff188621af7cc5bdae6fdffcd7516beca
Craig Disselkoen <cdiss@amazon.com>
2024-05-15 18:53:12 +0000

    nits
    Signed-off-by: Craig Disselkoen <cdiss@amazon.com>

--- cedar-lean/Cedar/Thm/Partial/Evaluation.lean
+++ cedar-lean/Cedar/Thm/Partial/Evaluation.lean
@@ -143,29 +143,40 @@ theorem on_concrete_gives_concrete (expr : Spec.Expr) (request : Spec.Request) (
   <;> trivial
 
 /--
-  subst on a Spec.Expr is id
+  subst on a concrete expression is that expression
 -/
-theorem subst_expr_id (subsmap : Map Unknown Partial.Value) (expr : Spec.Expr) :
+theorem subst_concrete_expr (expr : Spec.Expr) (subsmap : Map Unknown Partial.Value) :
   expr.asPartialExpr.subst subsmap = expr.asPartialExpr
 := by
-  unfold Partial.Expr.subst
-  cases expr <;> simp [Spec.Expr.asPartialExpr]
+  unfold Partial.Expr.subst Spec.Expr.asPartialExpr
+  cases expr
+  case lit | var => simp only
   case unaryApp op x₁ | getAttr x₁ attr | hasAttr x₁ attr =>
-    simp [subst_expr_id subsmap x₁]
+    simp only [Partial.Expr.unaryApp.injEq, Partial.Expr.getAttr.injEq, Partial.Expr.hasAttr.injEq, true_and, and_true]
+    exact subst_concrete_expr x₁ subsmap
   case and x₁ x₂ | or x₁ x₂ | binaryApp op x₁ x₂ =>
-    simp [subst_expr_id subsmap x₁, subst_expr_id subsmap x₂]
+    simp only [Partial.Expr.and.injEq, Partial.Expr.or.injEq, Partial.Expr.binaryApp.injEq, true_and, and_true]
+    constructor
+    · exact subst_concrete_expr x₁ subsmap
+    · exact subst_concrete_expr x₂ subsmap
   case ite x₁ x₂ x₃ =>
-    simp [subst_expr_id subsmap x₁, subst_expr_id subsmap x₂, subst_expr_id subsmap x₃]
+    simp only [Partial.Expr.ite.injEq]
+    repeat any_goals apply And.intro
+    · exact subst_concrete_expr x₁ subsmap
+    · exact subst_concrete_expr x₂ subsmap
+    · exact subst_concrete_expr x₃ subsmap
   case set xs | call xfn xs =>
-    simp [List.map₁_eq_map, List.map_map]
+    simp only [Partial.Expr.set.injEq, Partial.Expr.call.injEq, true_and, and_true]
+    simp only [List.map₁_eq_map, List.map_map]
     apply List.map_congr
     intro x _
-    exact subst_expr_id subsmap x
+    exact subst_concrete_expr x subsmap
   case record attrs =>
-    simp [List.map_attach₂_snd]
+    simp only [Partial.Expr.record.injEq, Partial.Expr.record.injEq, true_and, and_true]
+    simp only [List.map_attach₂_snd, List.map_map]
     apply List.map_congr
     intro (a, x) h₁
-    simp
+    simp only [Function.comp_apply, Prod.mk.injEq, true_and]
     have := List.sizeOf_snd_lt_sizeOf_list h₁
-    exact subst_expr_id subsmap x
+    exact subst_concrete_expr x subsmap
 termination_by expr
