5619e13ef6fdc34ca48160756dfc2734a50f8a72
Vasu Vikram <vasumvik@amazon.com>
2024-08-23 00:44:30 +0000

    update bolero random script

--- cedar-drt/cedar-bolero/run_bolero.sh
+++ cedar-drt/cedar-bolero/run_bolero.sh
@@ -6,16 +6,22 @@ CONFIG="$1" # one of "random", "libfuzzer"
 BENCHMARK="$2"
 REP="$3"
 TIME="$4"
-SEED_CORPUS="$4"
+BRANCH="$5"
 
 if [ -z "$CONFIG" ]; then
-    echo "Please set FUZZ_CONFIG env vars" > /dev/stderr
+    echo "USAGE: ./run_bolero.sh CONFIG BENCHMARK REP TIME BRANCH" > /dev/stderr
     exit 1
 fi
 
-git pull
+if [ "$BRANCH" == "vasu/derive_arbitrary_generators"  ]; then
+  (cd ../../cedar && git checkout $BRANCH)
+else 
+  (cd ../../cedar && git checkout vasu/well_typed_generators)
+fi
+
+git checkout $BRANCH
+git pull origin $BRANCH
 PATH=$PATH:/home/ubuntu/.elan/bin:/home/ubuntu/.cargo/bin
-(cd ../ && ./initialize_corpus.sh convert-policy-json-to-cedar)
 
 RESULTS_DIR="results/$BENCHMARK/$CONFIG/rep_$REP"
 CORPUS_DIR=$RESULTS_DIR/corpus
@@ -33,6 +39,10 @@ if [ "$BENCHMARK" == "raw_convert_policy_est_to_cedar"  ]; then
 fi
 
 source ../set_env_vars.sh
-echo 'cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1 -fork=1"'
-cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1 -fork=1"
-cp fuzz-0.log $RESULTS_DIR/fuzz.log
+if [ "$CONFIG" == "random"  ]; then
+    echo cargo bolero test $BENCHMARK --engine random -T $TIME 1> $RESULTS_DIR/valid.log
+    cargo bolero test $BENCHMARK --engine random -T $TIME 1> $RESULTS_DIR/valid.log
+else
+    echo cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1 -fork=1"
+    cp fuzz-0.log $RESULTS_DIR/fuzz.log
+fi
