840b3df5001cf4512e98b3dbb4416910109b30ed
John Kastner <jkastner@amazon.com>
2025-01-15 17:12:16 +0000

    fmt in fuzz dir
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-drt/fuzz/src/lib.rs
+++ cedar-drt/fuzz/src/lib.rs
@@ -249,7 +249,9 @@ pub fn run_val_test(
         TestResult::Failure(err) => {
             // TODO(#175): For now, ignore cases where the Lean code returned an error due to
             // an unknown extension function.
-            if !err.contains("unknown extension function") && !err.contains("unknown extension type") {
+            if !err.contains("unknown extension function")
+                && !err.contains("unknown extension type")
+            {
                 panic!(
                     "Unexpected error\nPolicies:\n{}\nSchema:\n{:?}\nError: {err}",
                     &policies, schema
