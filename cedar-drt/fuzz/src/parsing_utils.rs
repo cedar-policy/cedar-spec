55ef9e22dc321579afd377cdbd2eaadb4ef04ff7
Katherine Hough <kmhough@amazon.com>
2025-03-05 22:21:22 +0000

    * Updated convert-policy-json-to-cedar to operated on policy sets instead of policies
    Signed-off-by: Katherine Hough <kmhough@amazon.com>

--- cedar-drt/fuzz/src/parsing_utils.rs
+++ cedar-drt/fuzz/src/parsing_utils.rs
@@ -14,10 +14,14 @@
  * limitations under the License.
  */
 
-use cedar_policy_core::ast::{AnyId, Template};
+use cedar_policy_core::ast::{
+    ActionConstraint, AnyId, Effect, Expr, PolicySet, PrincipalConstraint, ResourceConstraint,
+    Template,
+};
 use cedar_policy_core::parser::err::{ParseError, ParseErrors, ToASTErrorKind};
 use smol_str::SmolStr;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::hash::{Hash, Hasher};
 
 // Check that two policies are equivalent, ignoring policy ids and source
 // locations.  Panic if the two policies are not the same.
@@ -60,4 +64,70 @@ pub fn check_for_internal_errors(errs: ParseErrors) {
         "Parse errors included unexpected internal errors: {:?}",
         miette::Report::new(errs).wrap_err("unexpected internal error")
     )
+}
+
+// Represents the aspects of a template that are preserved through conversions
+// to different representations.
+#[derive(Eq, Debug)]
+struct PreservedTemplate<'a> {
+    effect: Effect,
+    principal_constraint: &'a PrincipalConstraint,
+    action_constraint: &'a ActionConstraint,
+    resource_constraint: &'a ResourceConstraint,
+    annotations: BTreeMap<&'a AnyId, &'a SmolStr>,
+    non_scope_constraints: &'a Expr,
+}
+
+impl<'a> PartialEq for PreservedTemplate<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.effect == other.effect
+            && self.principal_constraint == other.principal_constraint
+            && self.action_constraint == other.action_constraint
+            && self.resource_constraint == other.resource_constraint
+            && self.annotations == other.annotations
+            && self
+                .non_scope_constraints
+                .eq_shape(other.non_scope_constraints)
+    }
+}
+
+impl<'a> Hash for PreservedTemplate<'a> {
+    fn hash<H: Hasher>(&self, hasher: &mut H) {
+        self.effect.hash(hasher);
+        self.principal_constraint.hash(hasher);
+        self.action_constraint.hash(hasher);
+        self.resource_constraint.hash(hasher);
+        self.annotations.hash(hasher);
+        self.non_scope_constraints.hash_shape(hasher);
+    }
+}
+
+impl<'a> From<&'a Template> for PreservedTemplate<'a> {
+    fn from(value: &'a Template) -> Self {
+        PreservedTemplate {
+            effect: value.effect(),
+            principal_constraint: value.principal_constraint(),
+            action_constraint: value.action_constraint(),
+            resource_constraint: value.resource_constraint(),
+            annotations: value.annotations().map(|(k, v)| (k, &v.val)).collect(),
+            non_scope_constraints: value.non_scope_constraints(),
+        }
+    }
+}
+
+// Checks that the static policies and templates in the specified
+// sets are equivalent, ignoring policy ids and source locations.
+// Panics if the two sets are not equivalent.
+pub fn check_policy_set_equivalence(set1: &PolicySet, set2: &PolicySet) {
+    // Convert templates in each set to preserved templates
+    let t1 = set1
+        .all_templates()
+        .map(PreservedTemplate::from)
+        .collect::<HashSet<PreservedTemplate>>();
+    let t2 = set2
+        .all_templates()
+        .map(PreservedTemplate::from)
+        .collect::<HashSet<PreservedTemplate>>();
+    // Check that the sets are the same size
+    assert_eq!(t1, t2);
 }
