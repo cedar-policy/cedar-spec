3ea3d2069a49848a2db7d22b6e07833dcf3543ba
John Kastner <jkastner@amazon.com>
2025-03-07 19:05:08 +0000

    fix
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-lean/Cedar/Thm/Validation.lean
+++ cedar-lean/Cedar/Thm/Validation.lean
@@ -21,7 +21,6 @@ import Cedar.Thm.Validation.Levels
 import Cedar.Thm.Validation.Slice
 import Cedar.Thm.Validation.Validator
 import Cedar.Thm.Validation.RequestEntityValidation
-import Cedar.Thm.Validation.Levels
 
 /-!
 This file contains the top-level correctness properties for the Cedar validator.
