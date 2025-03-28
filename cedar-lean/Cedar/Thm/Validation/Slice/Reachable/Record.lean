af9b54b642469c26cc7ca499489e3b001a9761ca
John Kastner <jkastner@amazon.com>
2025-03-28 19:09:06 +0000

    record
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-lean/Cedar/Thm/Validation/Slice/Reachable/Record.lean
+++ cedar-lean/Cedar/Thm/Validation/Slice/Reachable/Record.lean
@@ -28,6 +28,29 @@ open Cedar.Data
 open Cedar.Spec
 open Cedar.Validation
 
+theorem typed_record_contains_typed_attrs {rxs : List (Attr × Expr)} {a : Attr} {x : Expr} {c c' : Capabilities} {env : Environment}
+  (ht : typeOf (Expr.record rxs) c env = Except.ok (tx, c'))
+  (hx : (Map.make rxs).find? a = some x) :
+  ∃ rtxs ty atx,
+    tx = .record rtxs ty ∧
+    (Map.make rtxs).find? a = some atx ∧
+    ∃ c', typeOf x c env = .ok (atx, c')
+:= by
+  have ⟨_, _, htx, hfat⟩ := type_of_record_inversion ht
+  have ⟨_, _, hatx, ht''⟩ := find_make_xs_find_make_txs hfat hx
+  simp [htx, hatx, ht'']
+
+theorem record_entity_access_implies_attr_entity_access {atx : TypedExpr} {rtxs : List (Attr × TypedExpr)} {ty : CedarType} {n nmax : Nat} {a : Attr} {path : List Attr}
+  (hl : (TypedExpr.record rtxs ty).EntityAccessAtLevel env n nmax (a :: path))
+  (hatx : (Map.make rtxs).find? a = some atx) :
+  atx.EntityAccessAtLevel env n nmax path
+:= by
+  cases hl
+  rename_i atx' _ hfatx' hl
+  have hatx : atx' = atx := by
+    simpa [hfatx'] using hatx
+  simpa [hatx] using hl
+
 theorem checked_eval_entity_reachable_record {rxs : List (Attr × Expr)} {n : Nat} {c c' : Capabilities} {tx : TypedExpr} {env : Environment} {entities : Entities} {path : List Attr}
   (hc : CapabilitiesInvariant c request entities)
   (hr : RequestAndEntitiesMatchEnvironment env request entities)
@@ -39,34 +62,12 @@ theorem checked_eval_entity_reachable_record {rxs : List (Attr × Expr)} {n : Na
   (ih : ∀ a x, (Map.make rxs).find? a = some x → CheckedEvalEntityReachable x) :
   ReachableIn entities request.sliceEUIDs euid (n + 1)
 := by
-  have ⟨ hc', rtxs, htx, hfat ⟩ := type_of_record_inversion ht
-  subst hc' htx
-
   have ⟨ rvs, hv ⟩ := record_produces_record_value he
   subst hv
   cases ha
   rename_i v a path' hfv hv
   have ⟨ x, hfx, hex ⟩ := record_value_contains_evaluated_attrs he hfv
-
-  have ⟨atx, hfatx, het⟩ : ∃ atx, (Map.make rtxs).find? a = some atx ∧ AttrExprHasAttrType c env (a, x) (a, atx)  := by
-    have he' := Map.make_mem_list_mem (Map.find?_mem_toList hfx)
-    replace hfat' := List.forall₂_implies_all_left hfat
-    have ⟨ atx, _, haty ⟩ := hfat' (a, x) he'
-    have ⟨_, _, _, ht''⟩ := find_make_xs_find_make_txs hfat hfx
-    rename_i tx _ _
-    have ⟨ ha, htx ⟩ : a = atx.fst ∧ tx = atx.snd := by
-      simpa [AttrExprHasAttrType, ht''] using haty
-    subst ha htx
-    exists atx.snd
-
-  replace ⟨_, het⟩ : ∃ c', typeOf x c env = Except.ok (atx, c') := by
-    simpa [AttrExprHasAttrType] using het
-
-  have hl' : atx.EntityAccessAtLevel env n nmax path' := by
-    cases hl
-    rename_i atx' _ hfatx' hl
-    have hatx : atx' = atx := by
-      simpa [hfatx'] using hfatx
-    simpa [hatx] using hl
-
+  have ⟨ rtxs, _, atx, htx, hfatx, _, het⟩ := typed_record_contains_typed_attrs ht hfx
+  rw [htx] at hl
+  have hl' := record_entity_access_implies_attr_entity_access hl hfatx
   exact ih a x hfx hc hr het hl' hex hv hf
