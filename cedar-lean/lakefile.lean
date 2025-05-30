32320865b74deadeab184aef6f1c448a14449aaa
Emina Torlak <torlaket@amazon.com>
2025-05-30 11:07:39 -0700

    Change 'checkThm' command to check for imports in either Cedar/Thm.lean or SymCC.lean.
    Signed-off-by: Emina Torlak <torlaket@amazon.com>

--- cedar-lean/lakefile.lean
+++ cedar-lean/lakefile.lean
@@ -69,12 +69,13 @@ USAGE:
 @[lint_driver]
 script checkThm do
   let thm ← IO.FS.readFile ⟨"Cedar/Thm.lean"⟩
+  let symcc ← IO.FS.readFile ⟨"SymCC.lean"⟩
   let dir ← System.FilePath.readDir ⟨"Cedar/Thm/"⟩
   for entry in dir.toList do
     let fn := entry.fileName
     if fn.endsWith ".lean" then
-      let ln := s!"import Cedar.Thm.{fn.dropRight 5}"
-      if thm.replace ln "" == thm then
-        IO.println s!"Cedar.Thm does not import Cedar/Thm/{fn}"
+      let ln := s!"import Cedar.Thm.{fn.dropRight 5}\n"
+      if thm.replace ln "" == thm && symcc.replace ln "" == symcc then
+        IO.println s!"Neither Cedar.Thm nor SymCC imports Cedar/Thm/{fn}"
         return 1
   return 0
