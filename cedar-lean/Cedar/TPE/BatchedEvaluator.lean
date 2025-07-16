998222af2a16a119675a8a87b48ffce4a5e42921
Oliver Flatt <oflatt@gmail.com>
2025-07-16 12:32:28 -0700

    implemented but have termination issues
    Signed-off-by: Oliver Flatt <oflatt@gmail.com>

--- cedar-lean/Cedar/TPE/BatchedEvaluator.lean
+++ cedar-lean/Cedar/TPE/BatchedEvaluator.lean
@@ -38,19 +38,44 @@ def find_next_batch (x : Residual) : Set EntityUID :=
   | .call _ args _ => args.foldl (λ acc r => acc ∪ find_next_batch r) Set.empty
   | .error _ => Set.empty
 
+
+-- A computable size of residuals, not including the size of types
+def Residual.size (r: Residual): Nat :=
+ match r with
+  | .val v ty => 1
+  | .var v ty => 1
+  | .ite cond thenExpr elseExpr ty => 1 + Residual.size cond + Residual.size thenExpr + Residual.size elseExpr
+  | .and a b ty => 1 + Residual.size a + Residual.size b
+  | .or a b ty => 1 + Residual.size a + Residual.size b
+  | .unaryApp op expr ty => 1 + Residual.size expr
+  | .binaryApp op a b ty => 1 + Residual.size a + Residual.size b
+  | .getAttr expr attr ty => 1 + Residual.size expr + Residual.size attr
+  | .hasAttr expr attr ty => 1 + Residual.size expr + Residual.size attr
+  | .set ls ty => ls.foldl (λ s r => s + r.size) 1
+  | .record map ty => map.foldl (λ s kv => s + kv.2.size) 1
+  | .call xfn args ty => args.foldl (λ s r => s + r.size) 1
+  | .error ty => 1
+
+
 def batched_eval_loop
   (res : Residual)
   (req: Request)
   (loader: EntityLoader)
-  (store: PartialEntities)
+  (store: Entities)
   : Result Value :=
-  let to_load := (find_next_batch x).filter (λ uid => (store.get uid PartialEntityData.attrs).isNone)
+  let to_load := (find_next_batch res).filter (λ uid => (store.find? uid).isNone)
   let new_entities := loader to_load
-  let new_store := new_entities.kvs.foldl (λ acc ed => acc.insert ed.1 (EntityData.asPartialEntityData ed.2)) store
+  let new_store := new_entities.kvs.foldl (λ acc ed => acc.insert ed.1 ed.2) store
 
-  let new_res := Cedar.TPE.evaluate res (Request.asPartialRequest req) new_store
-
-  sorry
+  do
+    let expr ← res.asTypedExpr
+    let new_res := Cedar.TPE.evaluate expr (Request.asPartialRequest req) (Entities.asPartial new_store)
+    match new_res.asValue with
+    | some v => .ok v
+    | none =>
+      if new_res.size < res.size
+      then batched_eval_loop new_res req loader new_store
+      else Cedar.Spec.evaluate expr.toExpr req new_store
 
 def batched_evaluate
   (x : TypedExpr)
@@ -58,8 +83,8 @@ def batched_evaluate
   (loader: EntityLoader)
   : Result Value
   :=
-  let empty_store : PartialEntities := Map.mk []
+  let empty_store : Entities := Map.mk []
   -- do a first partial evaluation on x
-  let residual := Cedar.TPE.evaluate x (Request.asPartialRequest req) empty_store
+  let residual := Cedar.TPE.evaluate x (Request.asPartialRequest req) (Entities.asPartial empty_store)
   -- start the batching loop
   batched_eval_loop residual req loader empty_store
