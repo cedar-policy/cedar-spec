d91de3f08e4423ddc4c6adeabd0f58a17eaf68fe
Oliver Flatt <oflatt@gmail.com>
2025-07-16 16:32:12 -0700

    move to exprs more
    Signed-off-by: Oliver Flatt <oflatt@gmail.com>

--- cedar-lean/Cedar/TPE/BatchedEvaluator.lean
+++ cedar-lean/Cedar/TPE/BatchedEvaluator.lean
@@ -21,11 +21,11 @@ open Cedar.Validation
 
 def EntityLoader : Type := Set EntityUID → Map EntityUID EntityData
 
-def Residual.allLiteralUIDs (x : Residual) : Set EntityUID :=
+def TypedExpr.allLiteralUIDs (x : TypedExpr) : Set EntityUID :=
   match x with
-  | .val v _ty =>
-    match v with
-    | .prim (.entityUID uid) => Set.singleton uid
+  | .lit l _ =>
+    match l with
+    | .entityUID id => Set.singleton id
     | _ => Set.empty
   | .var v _ =>
     -- these cases should not happen, since the request was fully concrete
@@ -34,17 +34,16 @@ def Residual.allLiteralUIDs (x : Residual) : Set EntityUID :=
     | .resource  => Set.empty
     | .action    => Set.empty
     | .context   => Set.empty
-  | .ite c t e _ => c.allLiteralUIDs ∪ t.allLiteralUIDs ∪ e.allLiteralUIDs
-  | .and a b _   => a.allLiteralUIDs ∪ b.allLiteralUIDs
-  | .or a b _    => a.allLiteralUIDs ∪ b.allLiteralUIDs
-  | .unaryApp _ e _ => e.allLiteralUIDs
-  | .binaryApp _ a b _ => a.allLiteralUIDs ∪ b.allLiteralUIDs
-  | .getAttr e _ _ => e.allLiteralUIDs
-  | .hasAttr e _ _ => e.allLiteralUIDs
-  | .set ls _ => ls.mapUnion₁ (λ ⟨v, _⟩ => v.allLiteralUIDs)
-  | .record m _ => m.mapUnion₂ (λ ⟨⟨_attr, v⟩, _⟩ => v.allLiteralUIDs)
-  | .call _ ls _ => ls.mapUnion₁ (λ ⟨v, _⟩ => v.allLiteralUIDs)
-  | .error _ => Set.empty
+  | .ite c t e _ => TypedExpr.allLiteralUIDs c ∪ TypedExpr.allLiteralUIDs t ∪ TypedExpr.allLiteralUIDs e
+  | .and a b _   => TypedExpr.allLiteralUIDs a ∪ TypedExpr.allLiteralUIDs b
+  | .or a b _    => TypedExpr.allLiteralUIDs a ∪ TypedExpr.allLiteralUIDs b
+  | .unaryApp _ e _ => TypedExpr.allLiteralUIDs e
+  | .binaryApp _ a b _ => TypedExpr.allLiteralUIDs a ∪ TypedExpr.allLiteralUIDs b
+  | .getAttr e _ _ => TypedExpr.allLiteralUIDs e
+  | .hasAttr e _ _ => TypedExpr.allLiteralUIDs e
+  | .set ls _ => ls.mapUnion₁ (λ ⟨v, _⟩ => TypedExpr.allLiteralUIDs v)
+  | .record m _ => m.mapUnion₂ (λ ⟨⟨_attr, v⟩, _⟩ => TypedExpr.allLiteralUIDs v)
+  | .call _ ls _ => ls.mapUnion₁ (λ ⟨v, _⟩ => TypedExpr.allLiteralUIDs v)
 termination_by sizeOf x
 decreasing_by
   repeat case _ =>
@@ -66,21 +65,20 @@ def List.sum [Add α] [OfNat α 0] (xs : List α) : α :=
   xs.foldl (· + ·) 0
 
 -- A computable size of residuals, not including the size of types
-def Residual.size (r : Residual) : Nat :=
+def TypedExpr.size (r : TypedExpr) : Nat :=
  match r with
-  | .val _v _ty => 1
+  | .lit _l _ty => 1
   | .var _v _ty => 1
-  | .ite cond thenExpr elseExpr ty => 1 + cond.size + thenExpr.size + elseExpr.size
-  | .and a b ty => 1 + a.size + b.size
-  | .or a b ty => 1 + a.size + b.size
-  | .unaryApp op expr ty => 1 + expr.size
-  | .binaryApp op a b ty => 1 + a.size + b.size
-  | .getAttr expr attr ty => 1 + expr.size
-  | .hasAttr expr attr ty => 1 + expr.size
-  | .set ls ty => (ls.map₁ (λ ⟨v, _⟩ => v.size)).sum + 1
-  | .record map ty => (map.map₂ (λ ⟨⟨_attr, v⟩, _⟩ => v.size)).sum + 1
-  | .call xfn args ty => (args.map₁ (λ ⟨v, _⟩ => v.size)).sum + 1
-  | .error _ty => 1
+  | .ite cond thenExpr elseExpr ty => 1 + TypedExpr.size cond + TypedExpr.size thenExpr + TypedExpr.size elseExpr
+  | .and a b ty => 1 + TypedExpr.size a + TypedExpr.size b
+  | .or a b ty => 1 + TypedExpr.size a + TypedExpr.size b
+  | .unaryApp op expr ty => 1 + TypedExpr.size expr
+  | .binaryApp op a b ty => 1 + TypedExpr.size a + TypedExpr.size b
+  | .getAttr expr attr ty => 1 + TypedExpr.size expr
+  | .hasAttr expr attr ty => 1 + TypedExpr.size expr
+  | .set ls ty => (ls.map₁ (λ ⟨v, _⟩ => TypedExpr.size v)).sum + 1
+  | .record map ty => (map.map₂ (λ ⟨⟨_attr, v⟩, _⟩ => TypedExpr.size v)).sum + 1
+  | .call xfn args ty => (args.map₁ (λ ⟨v, _⟩ => TypedExpr.size v)).sum + 1
 termination_by sizeOf r
 decreasing_by
   repeat case _ =>
@@ -111,27 +109,25 @@ When no progress is made, one of these cases must be true:
   - An entity or entity field is missing, so the expression will error
 -/
 def batchedEvalLoop
-  (res : Residual)
+  (expr : TypedExpr)
   (req : Request)
   (loader : EntityLoader)
   (store : Entities)
   : Result Value :=
-  let to_load := res.allLiteralUIDs.filter (λ uid => (store.find? uid).isNone)
+  let to_load := (TypedExpr.allLiteralUIDs expr).filter (λ uid => (store.find? uid).isNone)
   let new_entities := loader to_load
   let new_store := new_entities.kvs.foldl (λ acc ed => acc.insert ed.1 ed.2) store
 
   do
-    let expr ← res.asTypedExpr
     let new_res := Cedar.TPE.evaluate expr (Request.asPartialRequest req) (Entities.asPartial new_store)
-    if new_res.size < res.size
-    then batchedEvalLoop new_res req loader new_store
+    let new_expr_res ← new_res.asTypedExpr
+    if TypedExpr.size new_expr_res < TypedExpr.size expr
+    then batchedEvalLoop new_expr_res req loader new_store
     else Cedar.Spec.evaluate expr.toExpr req new_store
 
-termination_by res.size
+termination_by TypedExpr.size expr
 decreasing_by
-  rename_i h
-  subst new_res
-  subst new_store
+  simp [TypedExpr.size]
   omega
 
 
@@ -149,4 +145,12 @@ def batchedEvaluate
   -- an initial partial evaluation, removing all variables
   let residual := Cedar.TPE.evaluate x (Request.asPartialRequest req) (Entities.asPartial empty_store)
   -- start the batched evaluation loop
-  batchedEvalLoop residual req loader empty_store
+  do
+    let new_expr ← residual.asTypedExpr
+    batchedEvalLoop new_expr req loader empty_store
+
+def entityLoaderFor : (e: Entities) -> EntityLoader :=
+  fun e =>
+   fun uids =>
+    Map.make (uids.toList.filterMap (fun uid =>
+      e.find? uid |>.map (fun data => (uid, data))))
