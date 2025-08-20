ba55cb115bfc3c3a70db7c3200cd4707b8d4fc5d
Oliver Flatt <oflatt@gmail.com>
2025-08-20 09:22:44 -0700

    moved another case
    Signed-off-by: Oliver Flatt <oflatt@gmail.com>

--- cedar-lean/Cedar/Thm/TPE/WellTypedCases.lean
+++ cedar-lean/Cedar/Thm/TPE/WellTypedCases.lean
@@ -47,7 +47,50 @@ abbrev PEWellTyped (env : TypeEnv)
   Residual.WellTyped env r₁ →
   Residual.WellTyped env r₂
 
--- Helper theorems for record key preservation
+-- Helper theorems for record key preservation and type lifting
+theorem find_lifted_type {attr ty₁ ty₂} {m: RecordType} :
+  Map.find? m attr = some ty₁ →
+  Map.find? m.liftBoolTypes attr = some ty₂ →
+  ty₂ = ty₁.liftBoolTypes
+:= by
+  simp [Map.find?, Map.kvs]
+  intro h₁ h₂
+  cases h₃: m.1
+  . rw [h₃] at h₁
+    simp at h₁
+  . rename_i hd tl
+    rw [h₃] at h₁
+    unfold RecordType.liftBoolTypes at h₂
+    rw [h₃] at h₂
+    simp [CedarType.liftBoolTypesRecord, List.find?] at h₂
+    cases h₄ : hd.fst == attr
+    case cons.false =>
+      rw [h₄] at h₂
+      simp at h₂
+      simp [List.find?] at h₁
+      rw [h₄] at h₁
+      simp at h₁
+      exact find_lifted_type h₁ h₂
+    case cons.true =>
+      rw [h₄] at h₂
+      simp at h₂
+      rw [← h₂]
+      simp [List.find?] at h₁
+      rw [h₄] at h₁
+      simp at h₁
+      rw [h₁]
+decreasing_by
+  have h₅ : sizeOf m.1 < sizeOf m := by {
+    simp [sizeOf]
+    simp [Map._sizeOf_1]
+    sorry
+  }
+  rename_i hd tail h₆ h₇
+  have h₈: sizeOf (Map.mk tail) < sizeOf m.1 := by {
+    sorry
+  }
+  omega
+
 theorem partial_eval_record_key_preservation_3 {ls : List (Attr × Residual)} :
   ls.find? (λ x => x.fst == k) = .some (k, v) →
   ∃ v₂,
@@ -730,6 +773,204 @@ theorem partial_eval_well_typed_record {env : TypeEnv} {ls : List (Attr × Resid
         simp
         let h₅ := partial_eval_preserves_typeof h_wf h_ref h₀
         rw [h₅]
+
+/--
+Helper theorem: Partial evaluation preserves well-typedness for getAttr residuals.
+-/
+theorem partial_eval_well_typed_getAttr {env : TypeEnv} {expr : Residual} {attr : Attr} {ty : CedarType} {req : Request} {preq : PartialRequest} {es : Entities} {pes : PartialEntities} :
+  Residual.WellTyped env (TPE.evaluate expr preq pes) →
+  InstanceOfWellFormedEnvironment req es env →
+  RequestAndEntitiesRefine req es preq pes →
+  Residual.WellTyped env (Residual.getAttr expr attr ty) →
+  Residual.WellTyped env (TPE.evaluate (Residual.getAttr expr attr ty) preq pes) := by
+  intros h_expr_wt h_wf h_ref h_wt
+  simp [TPE.evaluate, TPE.getAttr, TPE.attrsOf]
+  split
+  case h_1 =>
+    apply Residual.WellTyped.error
+  case h_2 r₁ h₁ =>
+    split
+    case h_1 x m h₂ =>
+      split at h₂
+      case h_1 r₂ m₂ ty₂ h₃ =>
+        injection h₂; rename_i h₂
+        rw [h₂] at h₃
+        cases h_wt
+        case getAttr_entity ety rty h₄ h₅ h₆ h₇ =>
+          have ih := h_expr_wt
+          rw [h₃] at ih
+          cases ih
+          rename_i h₇
+          cases h₇
+          rename_i rty₂ h₈ h₉ h₁₀
+          cases h₁₂ : m.find? attr
+          . simp [someOrError]
+            apply Residual.WellTyped.error
+          . rename_i v
+            simp [someOrError]
+            apply Residual.WellTyped.val
+            have h₁₁ := partial_eval_preserves_typeof h_wf h_ref h₅
+            rw [h₃] at h₁₁
+            rw [h₆] at h₁₁
+            simp [Residual.typeOf] at h₁₁
+        case getAttr_record rty h₄ h₅ h₆ =>
+          have ih := h_expr_wt
+          rw [h₃] at ih
+          cases ih
+          rename_i h₇
+          cases h₇
+          rename_i rty₂ h₈ h₉ h₁₀
+          cases h₁₂ : m.find? attr
+          . simp [someOrError]
+            apply Residual.WellTyped.error
+          . rename_i v
+            simp [someOrError]
+            apply Residual.WellTyped.val
+            have h₁₁ := partial_eval_preserves_typeof h_wf h_ref h₄
+            rw [h₃] at h₁₁
+            rw [h₅] at h₁₁
+            simp [Residual.typeOf] at h₁₁
+            cases h₁₃ : (Map.find? rty attr) <;> rw [h₁₃] at h₆
+            . simp at h₆
+            rename_i qty
+            simp at h₆
+            rw [h₁₁] at h₉
+            specialize h₉ attr v qty h₁₂ h₁₃
+            rw [h₆] at h₉
+            exact h₉
+      case h_2 r₂ uid ty₂ h₃ =>
+        cases h_wt
+        case getAttr_entity ety rty h₄ h₅ h₆ h₇ =>
+          have ih := h_expr_wt
+          rw [h₃] at ih
+          cases ih
+          rename_i h₇
+          cases h₇
+          rename_i ety₂ h₈
+          cases h₁₂ : m.find? attr
+          . simp [someOrError]
+            apply Residual.WellTyped.error
+          . rename_i v
+            simp [someOrError]
+            apply Residual.WellTyped.val
+            have h₁₁ := partial_eval_preserves_typeof h_wf h_ref h₅
+            rw [h₃] at h₁₁
+            rw [h₆] at h₁₁
+            simp [Residual.typeOf] at h₁₁
+            unfold RequestAndEntitiesRefine at h_ref
+            rcases h_ref with ⟨h_rref, h_eref⟩
+            unfold EntitiesRefine at h_eref
+            unfold PartialEntities.attrs PartialEntities.get at h₂
+            cases h₁₃ : (Map.find? pes uid) <;> rw [h₁₃] at h₂
+            . simp at h₂
+            . rename_i e
+              specialize h_eref uid e h₁₃
+              cases h_eref
+              . rename_i h₁₄
+                rcases h₁₄ with ⟨h₁₅, h₁₆⟩
+                rw [h₁₅] at h₂
+                simp [PartialEntityData.attrs] at h₂
+                rw [← h₂] at h₁₂
+                simp [Map.empty, Map.find?, Map.kvs, List.find?] at h₁₂
+              . rename_i h₁₄
+                rcases h₁₄ with ⟨e₂, h₁₄, h₁₅, h₁₆, h₁₇⟩
+                simp [Option.bind] at h₂
+                rw [h₂] at h₁₅
+                cases h₁₅
+                rename_i h₁₈
+                rw [h₁₈] at h₁₂
+                have h_wf2 := h_wf
+                unfold InstanceOfWellFormedEnvironment at h_wf2
+                rcases h_wf2 with ⟨h₁₉, h₂₀, h₂₁⟩
+                unfold InstanceOfSchema at h₂₁
+                rcases h₂₁ with ⟨h₂₁, h₂₂⟩
+                specialize h₂₁ uid e₂ h₁₄
+                unfold InstanceOfSchemaEntry at h₂₁
+                cases h₂₁
+                . rename_i h₂₃
+                  unfold InstanceOfEntitySchemaEntry at h₂₃
+                  rcases h₂₃ with ⟨e₃, h₂₃, h₂₄, h₂₅, h₂₆, h₂₇⟩
+                  unfold InstanceOfEntityType at h₈
+                  rcases h₈ with ⟨h₈, h₂₈⟩
+                  rw [← h₈] at h₂₃
+                  cases h₂₅
+                  rename_i h₂₉ h₃₀ h₃₁
+                  specialize h₃₀ attr v
+                  simp [EntitySchema.attrs?] at h₄
+                  rcases h₄ with ⟨e₄, h₃₂, h₃₃⟩
+                  rw [h₁₁] at h₂₃
+                  rw [h₂₃] at h₃₂
+                  injection h₃₂; rename_i h₃₂
+                  rw [← h₃₂] at h₃₃
+                  rw [← h₃₃] at h₇
+                  cases h₃₄ : (Map.find? e₃.attrs attr)
+                  . specialize h₂₉ attr
+                    simp [Map.contains] at h₂₉
+                    rw [h₁₂] at h₂₉
+                    simp at h₂₉
+                    rw [h₃₄] at h₂₉
+                    simp at h₂₉
+                  . rename_i ty₃
+                    cases h₃₅ : (Map.find? e₃.attrs.liftBoolTypes attr)
+                    . rw [h₃₅] at h₇
+                      simp at h₇
+                    . have h₃₆ := find_lifted_type h₃₄ h₃₅
+                      rename_i v₃
+                      rw [h₃₅] at h₇
+
+                      specialize h₃₀ ty₃ h₁₂ h₃₄
+                      simp at h₇
+                      rw [← h₇]
+                      rw [h₃₆]
+                      have h₃₇ := type_lifting_preserves_instance_of_type h₃₀
+                      cases ty₃
+                      all_goals
+                        rename_i ty₃
+                        simp [Qualified.getType] at h₃₇
+                        simp [QualifiedType.liftBoolTypes, Qualified.getType]
+                        exact h₃₇
+                . rename_i h₂₃
+                  unfold InstanceOfActionSchemaEntry at h₂₃
+                  rcases h₂₃ with ⟨e₃, h₂₃, h₂₄, h₂₅⟩
+                  rw [e₃] at h₁₂
+                  simp [Map.empty, Map.find?, Map.kvs] at h₁₂
+        case getAttr_record rty h₄ h₅ h₆ =>
+          have h₇ := partial_eval_preserves_typeof h_wf h_ref h₄
+          rw [h₃] at h₇
+          rw [h₅] at h₇
+          simp [Residual.typeOf] at h₇
+          have h₈ := h_expr_wt
+          rw [h₃] at h₈
+          cases h₈
+          rename_i h₈
+          rw [h₇] at h₈
+          cases h₈ -- contradiction
+      case h_3 =>
+        contradiction
+    case h_2 x h₂ =>
+      cases h_wt
+      case getAttr_entity ety rty h₅ h₆ h₇ h₈ =>
+        apply Residual.WellTyped.getAttr_entity
+        case h₁ =>
+          exact h_expr_wt
+        case h₂ =>
+          have h₁₀ := partial_eval_preserves_typeof h_wf h_ref h₆
+          rw [h₁₀]
+          rw [h₇]
+        case h₃ =>
+          rw [h₅]
+        case h₄ =>
+          exact h₈
+      case getAttr_record rty h₆ h₇ h₈ =>
+        apply Residual.WellTyped.getAttr_record
+        case h₁ =>
+          exact h_expr_wt
+        case h₂ =>
+          have h₁₀ := partial_eval_preserves_typeof h_wf h_ref h₆
+          rw [h₁₀]
+          rw [h₇]
+        case h₃ =>
+          rw [h₈]
 
 theorem partial_eval_well_typed_app₂ :
   Residual.WellTyped env (TPE.evaluate expr1 preq pes) →
