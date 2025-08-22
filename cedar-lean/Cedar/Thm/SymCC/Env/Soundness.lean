2173b3c04e07659340598b433cb73e60bca3a1e9
Oliver Flatt <oflatt@gmail.com>
2025-08-22 14:29:56 -0700

    fix build
    Signed-off-by: Oliver Flatt <oflatt@gmail.com>

--- cedar-lean/Cedar/Thm/SymCC/Env/Soundness.lean
+++ cedar-lean/Cedar/Thm/SymCC/Env/Soundness.lean
@@ -280,7 +280,7 @@ private theorem map_make_filterMap_find?
   (hf : ∀ kv, (∃ v'', f kv = .some (k', v'')) → kv.1 = k) :
   (Map.make (m.toList.filterMap f)).find? k' = .some v'
 := by
-  apply Map.list_find?_implies_make_find?
+  rw [← Map.list_find?_iff_make_find?]
   simp only [List.find?_filterMap]
   have :
     List.find? (fun a => Option.any (fun x => x.fst == k') (f a)) m.toList
@@ -316,7 +316,7 @@ private theorem map_make_filterMap_flatten_find?
   (hf : ∀ kv, (∃ l, f kv = .some l ∧ (l.find? (λ x => x.1 == k')).isSome) → kv.1 = k) :
   (Map.make (m.toList.filterMap f).flatten).find? k' = .some v'
 := by
-  apply Map.list_find?_implies_make_find?
+  rw [← Map.list_find?_iff_make_find?]
   simp only [List.find?_flatten]
   cases m with | mk l =>
   simp only [Map.toList, Map.kvs, Map.find?] at *
@@ -774,7 +774,7 @@ private theorem env_symbolize?_same_entity_data_standard
     exists UnaryFunction.interpret (env.symbolize? Γ) (SymEntityData.ofStandardEntityType.ancsUUF uid.ty anc.ty)
     constructor
     · apply Map.find?_mapOnValues_some
-      apply Map.list_find?_implies_make_find?
+      rw [← Map.list_find?_iff_make_find?]
       simp only [List.find?_map]
       unfold Function.comp
       simp only
@@ -1017,7 +1017,7 @@ private theorem env_symbolize?_same_entities_action
       (Set.make (List.filterMap (SymEntityData.ofActionType.termOfType? ancTy) entry.ancestors.toList))
       (TermType.entity ancTy))
   := by
-    apply Map.list_find?_implies_make_find?
+    rw [← Map.list_find?_iff_make_find?]
     simp only [List.find?_filterMap]
     have :
       (List.find?
