bff34fc95deb29237c2602da6ad294b2c22d0b82
John Kastner <jkastner@amazon.com>
2025-09-24 16:20:48 +0000

    Adjust level based entity slicing to be infallible
    This procedure would previously return `none` if it dereferenced an
    entity which did not exist in the original entities. It now returns an
    entity slice without the missing entity. This means that evaluating a
    request with the slice may result in an unknown entity error, but the
    soundness proofs shows that that this only happens when the error
    also occurred using the unsliced entities.
    
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-lean/Cedar/Thm/Validation/Levels/BinaryApp.lean
+++ cedar-lean/Cedar/Thm/Validation/Levels/BinaryApp.lean
@@ -47,39 +47,37 @@ theorem not_dereferencing_apply₂_invariant_entities {op : BinaryOp} {entities 
     ) <;>
     simp only
 
-theorem level_based_slicing_is_sound_inₑ {e₁ : Expr} {euid₁ euid₂ : EntityUID} {n : Nat} {c₀ c₁ : Capabilities} {entities slice : Entities}
+theorem level_based_slicing_is_sound_inₑ {e₁ : Expr} {euid₁ euid₂ : EntityUID} {n : Nat} {c₀ c₁ : Capabilities} {entities : Entities}
   (hc : CapabilitiesInvariant c₀ request entities)
   (hr : InstanceOfWellFormedEnvironment request entities env)
   (ht : typeOf e₁ c₀ env = Except.ok (tx₁, c₁))
   (hl : tx₁.EntityAccessAtLevel env n (n + 1) [])
   (he : evaluate e₁ request entities = .ok (Value.prim (Prim.entityUID euid₁)))
-  (hs : some slice = entities.sliceAtLevel request (n + 1))
-  : inₑ euid₁ euid₂ entities = inₑ euid₁ euid₂ slice
+  : inₑ euid₁ euid₂ entities = inₑ euid₁ euid₂ (entities.sliceAtLevel request (n + 1))
 := by
   simp only [inₑ]
   cases heq : euid₁ == euid₂ <;> simp only [Bool.false_or, Bool.true_or]
-  have hfeq := checked_eval_entity_find_entities_eq_find_slice hc hr ht hl he hs
+  have hfeq := checked_eval_entity_find_entities_eq_find_slice hc hr ht hl he
   simp [hfeq, Entities.ancestorsOrEmpty]
 
-theorem level_based_slicing_is_sound_binary_app {op : BinaryOp} {e₁ e₂ : Expr} {n : Nat} {c₀ c₁: Capabilities} {env : TypeEnv} {request : Request} {entities slice : Entities}
-  (hs : slice = entities.sliceAtLevel request n)
+theorem level_based_slicing_is_sound_binary_app {op : BinaryOp} {e₁ e₂ : Expr} {n : Nat} {c₀ c₁: Capabilities} {env : TypeEnv} {request : Request} {entities : Entities}
   (hc : CapabilitiesInvariant c₀ request entities)
   (hr : InstanceOfWellFormedEnvironment request entities env)
   (ht : typeOf (.binaryApp op e₁ e₂) c₀ env = Except.ok (tx, c₁))
   (hl : tx.AtLevel env n)
   (ihe₁ : TypedAtLevelIsSound e₁)
   (ihe₂ : TypedAtLevelIsSound e₂)
-  : evaluate (.binaryApp op e₁ e₂) request entities = evaluate (.binaryApp op e₁ e₂) request slice
+  : evaluate (.binaryApp op e₁ e₂) request entities = evaluate (.binaryApp op e₁ e₂) request (entities.sliceAtLevel request n)
 := by
   replace ⟨tx₁, c₁, tx₂, c₂, htx₁, htx₂, ty, htx⟩ := type_of_binaryApp_inversion ht
   subst tx
   simp only [evaluate]
   cases hl
   case getTag hel hl₁ hl₂ | hasTag hel hl₁ hl₂ =>
-    specialize ihe₂ hs hc hr htx₂ hl₂
+    specialize ihe₂ hc hr htx₂ hl₂
     rw [←ihe₂]
     have hl₁' := entity_access_at_level_then_at_level hl₁
-    specialize ihe₁ hs hc hr htx₁ hl₁'
+    specialize ihe₁ hc hr htx₁ hl₁'
     rw [←ihe₁]
     cases he₁ : evaluate e₁ request entities <;> simp only [Except.bind_ok, Except.bind_err]
     cases he₂ : evaluate e₂ request entities <;> simp only [Except.bind_ok, Except.bind_err]
@@ -88,13 +86,13 @@ theorem level_based_slicing_is_sound_binary_app {op : BinaryOp} {e₁ e₂ : Exp
     rename_i p₁ p₂
     cases p₁ <;> cases p₂ <;> simp only
     rename_i euid _
-    have hfeq := checked_eval_entity_find_entities_eq_find_slice hc hr htx₁ hl₁ he₁ hs
+    have hfeq := checked_eval_entity_find_entities_eq_find_slice hc hr htx₁ hl₁ he₁
     simp only [hfeq, hasTag, getTag, Entities.tagsOrEmpty, Entities.tags, Map.findOrErr]
   case mem hel hl₁ hl₂ =>
-    specialize ihe₂ hs hc hr htx₂ hl₂
+    specialize ihe₂ hc hr htx₂ hl₂
     rw [←ihe₂]
     have hl₁' := entity_access_at_level_then_at_level hl₁
-    specialize ihe₁ hs hc hr htx₁ hl₁'
+    specialize ihe₁ hc hr htx₁ hl₁'
     rw [←ihe₁]
     cases he₁ : evaluate e₁ request entities <;> simp only [Except.bind_ok, Except.bind_err]
     cases he₂ : evaluate e₂ request entities <;> simp only [Except.bind_ok, Except.bind_err]
@@ -103,13 +101,13 @@ theorem level_based_slicing_is_sound_binary_app {op : BinaryOp} {e₁ e₂ : Exp
     case prim =>
       rename_i p₁ p₂
       cases p₁ <;> cases p₂ <;> simp only
-      simp [level_based_slicing_is_sound_inₑ hc hr htx₁ hl₁ he₁ hs]
+      simp [level_based_slicing_is_sound_inₑ hc hr htx₁ hl₁ he₁]
     case set =>
       rename_i p₁ sv
       cases p₁ <;> simp only
-      simp [inₛ, level_based_slicing_is_sound_inₑ hc hr htx₁ hl₁ he₁ hs]
+      simp [inₛ, level_based_slicing_is_sound_inₑ hc hr htx₁ hl₁ he₁]
   case binaryApp hop hl₁ hl₂ =>
-    specialize ihe₁ hs hc hr htx₁ hl₁
-    specialize ihe₂ hs hc hr htx₂ hl₂
+    specialize ihe₁ hc hr htx₁ hl₁
+    specialize ihe₂ hc hr htx₂ hl₂
     rw [ihe₁, ihe₂]
-    simp [(@not_dereferencing_apply₂_invariant_entities op entities slice · · hop)]
+    simp [(@not_dereferencing_apply₂_invariant_entities op entities (entities.sliceAtLevel request n) · · hop)]
