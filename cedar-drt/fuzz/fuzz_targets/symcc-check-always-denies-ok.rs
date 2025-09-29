25e03c3c82d36965c582f13b7ee5bb93e0d6def0
Katherine Hough <kmhough@amazon.com>
2025-09-29 15:51:19 +0000

    * Added fuzzing targets that check that SymCC verification tasks performed on inputs that pass strict validations either timeout or produce a value. * Fixed potential resource leak from unreaped child processes in `symcc-cex-drt`.
    Signed-off-by: Katherine Hough <kmhough@amazon.com>

---
+++ cedar-drt/fuzz/fuzz_targets/symcc-check-always-denies-ok.rs
@@ -0,0 +1,95 @@
+/*
+ * Copyright Cedar Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![no_main]
+use cedar_drt::logger::initialize_log;
+
+use cedar_drt_inner::{
+    fuzz_target,
+    symcc::{total_action_request_env_limit, PolicySetTask, ValidationTask},
+};
+
+use cedar_policy::{PolicySet, Schema};
+
+use cedar_policy_generators::{
+    abac::ABACPolicy, hierarchy::HierarchyGenerator, schema, settings::ABACSettings,
+};
+
+use libfuzzer_sys::arbitrary::{self, Arbitrary, MaxRecursionReached, Unstructured};
+use std::convert::TryFrom;
+use std::sync::LazyLock;
+
+static RUNTIME: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
+
+/// Input expected by this fuzz target.
+#[derive(Debug, Clone)]
+pub struct FuzzTargetInput {
+    /// generated schema
+    pub schema: schema::Schema,
+    /// generated policy
+    pub policy: ABACPolicy,
+}
+
+/// Settings for this fuzz target.
+const SETTINGS: ABACSettings = ABACSettings {
+    match_types: true,
+    enable_extensions: true,
+    max_depth: 3,
+    max_width: 3,
+    enable_additional_attributes: false,
+    enable_like: true,
+    enable_action_groups_and_attrs: true,
+    enable_arbitrary_func_call: true,
+    enable_unknowns: false,
+    enable_action_in_constraints: true,
+    per_action_request_env_limit: ABACSettings::default_per_action_request_env_limit(),
+    total_action_request_env_limit: total_action_request_env_limit(),
+};
+
+impl<'a> Arbitrary<'a> for FuzzTargetInput {
+    fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+        let schema = schema::Schema::arbitrary(SETTINGS.clone(), u)?;
+        let hierarchy = schema.arbitrary_hierarchy(u)?;
+        let policy = schema.arbitrary_policy(&hierarchy, u)?;
+        Ok(Self { schema, policy })
+    }
+
+    fn try_size_hint(
+        depth: usize,
+    ) -> std::result::Result<(usize, Option<usize>), MaxRecursionReached> {
+        Ok(arbitrary::size_hint::and_all(&[
+            schema::Schema::arbitrary_size_hint(depth)?,
+            HierarchyGenerator::size_hint(depth),
+        ]))
+    }
+}
+
+fuzz_target!(|input: FuzzTargetInput| {
+    initialize_log();
+    let mut policy_set = PolicySet::new();
+    policy_set.add(input.policy.into()).unwrap();
+    // Attempt to convert the generator schema to an actual schema.
+    if let Ok(schema) = Schema::try_from(input.schema) {
+        RUNTIME
+            .block_on(PolicySetTask::CheckAlwaysDenies.check_ok(schema, policy_set))
+            .unwrap();
+    }
+});
