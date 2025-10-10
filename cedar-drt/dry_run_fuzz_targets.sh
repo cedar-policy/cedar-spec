89fec70c01a895a9b301770f197c3cc55f63ffd1
John Kastner <jkastner@amazon.com>
2025-10-10 14:34:53 +0000

    intall cargo fuzz
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-drt/dry_run_fuzz_targets.sh
+++ cedar-drt/dry_run_fuzz_targets.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Build all fuzz targets and run them for 100 iterations starting from a fixed
+# Build all fuzz targets and run them for 10 iterations starting from a fixed
 # seed.  Not intended to find bugs in the code under test, just to exercise the
 # targets and catch any silly mistakes in their implementation.
+#
+# If running locally, you might want to `rm -rf ./fuzz/corpus` since the fuzzer
+# will first run any existing corpus entries which may take a while,
+# particularly since this uses a debug build.
 
 set -e
 set -o pipefail
 
-cargo fuzz build -s none
-cargo fuzz list | xargs -I{} cargo fuzz run  -s none {} -- -seed=1 -runs=100
+cargo fuzz build -s none --dev
+cargo fuzz list | xargs -I{} cargo fuzz run -s none --dev {} -- -seed=1 -runs=10
