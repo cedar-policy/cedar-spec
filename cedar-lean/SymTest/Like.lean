c5c126bdb3683f9d2ab000df0d568bd893208609
Shaobo He <shaobohe@amazon.com>
2025-11-17 15:03:23 -0800

    updates
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- cedar-lean/SymTest/Like.lean
+++ cedar-lean/SymTest/Like.lean
@@ -37,9 +37,8 @@ private def x : Expr := .getAttr (.var .context) "x"
 private def y : Expr := .getAttr (.var .context) "y"
 private def z : Expr := .getAttr (.var .context) "z"
 
-private def justChars : String → Pattern
-  | .mk [] => []
-  | .mk (c :: cs) => (.justChar c) :: (justChars (.mk cs))
+private def justChars (str : String) : Pattern :=
+  str.data.map (fun c => .justChar c)
 
 def testsForBasicLikeOps :=
   suite "Like.basic"
