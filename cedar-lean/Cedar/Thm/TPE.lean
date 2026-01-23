d0b828edd64ba91065dcad27ef3b9d5660e0f2ea
John Kastner <jkastner@amazon.com>
2026-01-23 16:28:57 +0000

    rebase and add import
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-lean/Cedar/Thm/TPE.lean
+++ cedar-lean/Cedar/Thm/TPE.lean
@@ -23,6 +23,7 @@ import Cedar.Thm.TPE.Soundness
 import Cedar.Thm.TPE.PreservesTypeOf
 import Cedar.Thm.TPE.WellTyped
 import Cedar.Thm.TPE.WellTypedCases
+import Cedar.Thm.TPE.ErrorFree
 import Cedar.Thm.Validation
 
 /-!
