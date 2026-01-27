1aafca112fc367043c7d00d683a0bdbc2a562e14
Craig Disselkoen <cdiss@amazon.com>
2026-01-27 16:06:47 +0000

    fix comment formatting
    Signed-off-by: Craig Disselkoen <cdiss@amazon.com>

--- cedar-drt/tests/benchmark.rs
+++ cedar-drt/tests/benchmark.rs
@@ -163,11 +163,11 @@ fn print_summary(auth_times: HashMap<&str, Vec<f64>>, val_times: HashMap<&str, V
 }
 
 #[test]
-#[ignore] // doesn't test anything, just prints timing results -- run this on
-// demand but not in CI.
-// note that actual Lean functionality on the corpus tests is tested
-// by a test called `integration_tests_on_def_impl()`, in
-// integration_tests.rs.
+#[ignore]
+// doesn't test anything, just prints timing results -- run this on demand but
+// not in CI.
+// note that actual Lean functionality on the corpus tests is tested by a test
+// called `integration_tests_on_def_impl()`, in integration_tests.rs.
 fn print_timing_results() {
     let rust_impl = RustEngine::new();
     let lean_impl = CedarLeanEngine::new();
