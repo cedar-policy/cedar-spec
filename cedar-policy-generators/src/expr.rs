c8d9d1315d087cd9fcea4133128a4c40bdd46876
John Kastner <jkastner@amazon.com>
2026-02-05 16:34:03 +0000

    Generate comparisons using datetime/duration types
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-policy-generators/src/expr.rs
+++ cedar-policy-generators/src/expr.rs
@@ -365,58 +365,24 @@ impl ExprGenerator<'_> {
                                 u,
                             )?,
                         )),
-                        // < expression
-                        1 => Ok(ast::Expr::less(
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                        )),
-                        // <= expression
-                        1 => Ok(ast::Expr::lesseq(
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                        )),
-                        // > expression
-                        1 => Ok(ast::Expr::greater(
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                        )),
-                        // >= expression
-                        1 => Ok(ast::Expr::greatereq(
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                            self.generate_expr_for_type(
-                                &Type::long(),
-                                max_depth - 1,
-                                u,
-                            )?,
-                        )),
+                        // Binary comparisons expressions (>, >=, <, and <=)
+                        4 => {
+                            let cmp_ty = gen!(u,
+                                1 => Type::long(),
+                                1 => Type::datetime(),
+                                1 => Type::duration()
+                            );
+                            let cmp_op = gen!(u,
+                                1 => ast::Expr::greater,
+                                1 => ast::Expr::greatereq,
+                                1 => ast::Expr::less,
+                                1 => ast::Expr::lesseq
+                            );
+                            Ok(cmp_op(
+                                self.generate_expr_for_type(&cmp_ty, max_depth - 1, u)?,
+                                self.generate_expr_for_type(&cmp_ty, max_depth - 1, u)?,
+                            ))
+                        },
                         // in expression, non-set form
                         11 => {
                             let ety1 = self.schema.arbitrary_entity_type(u)?;
