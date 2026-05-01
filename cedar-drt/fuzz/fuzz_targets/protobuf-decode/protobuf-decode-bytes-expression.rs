4362b3354131d6394fb12b00b3edbb45e57d477b
Victor Nicolet <victornl@amazon.com>
2026-05-01 20:31:20 +0000

    fuzz targets for protobuf decode shall not panic
    Signed-off-by: Victor Nicolet <victornl@amazon.com>

---
+++ cedar-drt/fuzz/fuzz_targets/protobuf-decode/protobuf-decode-bytes-expression.rs
@@ -0,0 +1,28 @@
+/*
+ * Copyright Cedar Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![no_main]
+
+use cedar_drt_inner::fuzz_target;
+
+use cedar_policy::Expression;
+use cedar_policy::proto::traits::Protobuf;
+
+// Feed arbitrary bytes into Expression protobuf decoder.
+// The property under test: decode either returns Ok or Err, never panics.
+fuzz_target!(|input: &[u8]| {
+    let _ = Expression::decode(input);
+});
