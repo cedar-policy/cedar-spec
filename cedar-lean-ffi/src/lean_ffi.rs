3d933a5ee5169bdc51a6616aa0a914e3f719fcd6
Liana Hadarean <hadarean@amazon.com>
2026-08-14 17:23:18 +0000

    Removed one more instance of BatteriesRecycling from lean ffi.
    Signed-off-by: Liana Hadarean <hadarean@amazon.com>

--- cedar-lean-ffi/src/lean_ffi.rs
+++ cedar-lean-ffi/src/lean_ffi.rs
@@ -1294,7 +1294,6 @@ mod test {
     #[link(name = "Cedar_Protobuf", kind = "static")]
     #[link(name = "Cedar_CedarProto", kind = "static")]
     #[link(name = "batteries_Batteries", kind = "static")]
-    #[link(name = "batteries_BatteriesRecycling", kind = "static")]
     #[link(name = "Cedar_CedarFFI", kind = "static")]
     unsafe extern "C" {}
 
